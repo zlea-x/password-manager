1f57de2eca35f11e47ef34e6b9c6dad7c9bcb768
Azalea Akpokugbe <akpokugbea01@gmail.com>
2025-05-11 13:55:40 +0100

    Test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#the main file that runs the program
+
+#PasswordEntry test
 from password_entry import PasswordEntry
 
 def main():
@@ -12,7 +15,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-
+#Encryption test
 from encryption_util import EncryptionUtil
 def test_encryption():
     text = "Firstclass5.0"
@@ -24,3 +27,29 @@ def test_encryption():
 
 if __name__ == "__main__":
     test_encryption()
+
+#PasswordManager test
+from password_entry import PasswordEntry
+from password_manager import PasswordManager
+
+def main():
+    manager = PasswordManager()
+
+    #Adding a new password
+    entry = PasswordEntry("Amazon.com", "anuri_mena", "welov3shopp1ng")
+    manager.add_password(entry)
+
+    #getting the password
+    result = manager.get_password("Amazon.com")
+    if result:
+        print(f"Found: {result.website} - {result.username} - {result.password}")
+    else:
+        print("Not found")
+
+    result = manager.get_password("Zara.com")
+    if result:
+        print(f"Found: {result.website} - {result.username} - {result.password}")
+    else:
+        print("Not found")
+
+    
--- password_manager.py
+++ password_manager.py
@@ -14,5 +14,58 @@ class PasswordManager:
         """
         Adds a new password entry objest to the manager.
         """
-        self.passwords.append(entry)
+        self.passwords.append(entry)  # adds the entry to the list
+        self.save_to_file() # saves the updated list to the file
+
+    def get_password(self, website):
+        """
+        gets a password for an inputed website
+        """
+        for entry in self.passwords:
+            if entry.website == website:
+                return entry #returs the password if the website matches
+        return None
+    
+    def update_password(self, website, username, password):
+        for entry in self.passwords:
+            if entry.website == website:
+                entry.username = username
+                entry.password = password
+                self.save_to_file()
+                return True
+        return False
+    
+    def delete_password(self, website):
+        self.passwords = [entry for entry in self.passwords if entry.website != website]
         self.save_to_file()
+
+    def save_to_file(self):
+        """
+        Convert the list of password entries to JSON, encrypt it, and saves it to a file.
+        """
+        data = [entry.to_dict() for entry in self.passwords]  # Convert each object to a dictionary
+        json_data = json.dumps(data)                   # Convert the list of dictionaries to a JSON string
+        encrypted = EncryptionUtil.encrypt(json_data)  # Encrypt the JSON string using encryption utility
+
+        with open(self.filename, 'wb') as key_file:           # (wb) Open file in binary write mode
+            key_file.write(encrypted)                         # Writes the encrypted data to file
+
+    def load_from_file(self):
+        """
+        Load and decrypt data from file (if it exists).
+        Converts decrypted JSON back into PasswordEntry objects.
+        """
+        if not os.path.exists(self.filename):
+            return                                     # checks and exit if file doesn't exist
+
+        with open(self.filename, 'rb') as key_file:           # (rb) Open file in binary read mode
+            encrypted = key_file.read()                       # Read the encrypted data
+            try:
+                decrypted = EncryptionUtil.decrypt(encrypted)     # trying to decrypt the data using the utility class
+                data = json.loads(decrypted)                     # Load JSON string into Python list if the decryption is successful
+                # Convert each dictionary back into a PasswordEntry object
+                self.passwords = [PasswordEntry.from_dict(entry) for entry in data]
+            except Exception as e:
+                # Handle decryption or JSON errors
+                print("Error loading data: {e}")
+                self.passwords = []                    # If there's an error, use an empty list
